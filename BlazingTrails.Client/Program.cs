7420fbfd63e2a859a873ad05ece252b6090d84de
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-16 23:53:58 -0400

    configuring Blazor WebAssembly to use Auth0

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -8,5 +8,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 await builder.Build().RunAsync();
