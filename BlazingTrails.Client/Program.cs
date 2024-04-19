2aa5804043c5c60491a695b27717e974c80cbc36
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-19 00:22:22 -0400

    Replace HttpClient with IHttpClientFactory
    The HttpClient instances have been replaced with the use of IHttpClientFactory across multiple parts of the application. This change provides benefits such as having a central place for naming and configuring logical HttpClient instances as needed, offering the ability to manage the lifetime of the underlying HttpMessageHandler instances automatically.
    
    Currently storing/editing does not seem to be working for unknown reasons

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingTrails.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddHttpClient("SecureApiClient", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
