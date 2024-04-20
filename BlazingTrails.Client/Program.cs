4558d45bf5d732d40c6f47a074e94eeb6599ff3f
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-20 16:37:27 -0400

    improving the AppState design to handle more state - Part 2: and Blazored.LocalStorage

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingTrails.Client;
 using BlazingTrails.Client.State;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddScoped<AppState>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
