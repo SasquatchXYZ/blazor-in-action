73607aec4e1967f301b69324ebcef3f74f72280b
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-04-20 15:50:03 -0400

    add AppState and register it

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingTrails.Client;
+using BlazingTrails.Client.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddScoped<AppState>();
 
 await builder.Build().RunAsync();
